d4724bb5fb90689b9444f97e9799933b1c2937b9
Kysha <33337655+llinzy@users.noreply.github.com>
2020-08-31 21:46:57 -0400

    Add files via upload

---
+++ interpretation.py
@@ -0,0 +1,43 @@
+import pandas as pd
+import streamlit as st
+import numpy as np
+import plotly.express as px
+
+
+data=pd.read_csv('rf_data.csv')
+cfmatx=pd.read_csv('rf_confusion_matrix.csv')
+metric_df=pd.read_csv('metric_df.csv')
+top5_df=pd.read_csv('top5_df.csv')
+
+st.write("""
+Interpretations for the Random Forest Credit Data Predictions
+""")
+
+st.subheader('Results')
+default_type=st.multiselect('Select a Default Type', data.DEFAULTS.unique(), default=None)
+new_data=data[data.DEFAULTS.isin(default_type)]
+st.write(new_data.iloc[:,1:])
+
+st.subheader('Chart')
+pred=st.selectbox('X', data.columns[0:3])
+deft=st.selectbox('Y', data.columns[0:3])
+fig = px.scatter(new_data, x =pred,y=deft, color='DEFAULTS')
+st.plotly_chart(fig)
+
+st.subheader('Confusion Matrix')
+st.write(cfmatx.iloc[:,1:])
+
+st.subheader('Metrics')
+st.write(metric_df.iloc[:,1:])
+
+st.subheader('Top 5 Features by Importance')
+st.write(top5_df.iloc[:,1:])
+
+st.write('''The illustration above shows that the predictions for 
+positive defaults has greater probablity strengths supporting 
+a higher level of confidence in determining the applicant profiles
+more likely to default on a credit loan.  Specifically, applicants 
+with no checking account or a zero or less checking account balance, and/or
+critical account history and applying for a new car loan
+or a furniture/equipment loan is more likely to default on a credit
+agreement''')
